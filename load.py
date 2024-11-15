ea7632027a42c1e08c2fc799e238c1ee7662cb93
Linn Eriksson <linn@Linns-MacBook-Pro.local>
2024-11-15 18:19:40 -0500

    If files_dp dont exist in --path, check if data directory exist and read in and save the dustPy data

--- load.py
+++ load.py
@@ -1,4 +1,6 @@
 import numpy as np
+import os
+import re
 
 import glob
 
@@ -15,15 +17,58 @@ class Model:
         self.limit()
     
     def load(self):
-        self.r = np.load(self.path+'/files_dp/r.npy')
-        self.t = np.load(self.path+'/files_dp/tdustev.npy')
-        self.sigma_gas = np.load(self.path+'/files_dp/sigma_gas_2D.npy') # (t,r)
-        self.sigma_dust_poly = np.load(self.path+'/files_dp/sigma_dust_poly_3D.npy') # (t,r,fluid-bins)
-        self.rho_gas = np.load(self.path+'/files_dp/rho_gas_2D.npy') # (t,r)
-        self.rho_dust_poly = np.load(self.path+'/files_dp/rho_dust_3D.npy') # (t,r,fluid-bins)
-        self.St_poly = np.load(self.path+'/files_dp/st_poly_3D.npy') # (t,r,fluid-bins)
-        self.temp = np.load(self.path+'/files_dp/temp_2D.npy') # (t,r)
-        self.vrad_dust_poly = np.load(self.path+'/files_dp/vrad_dust_3D.npy') # (t,r,fluid-bins)
+        if os.path.exists(os.path.join(self.path, 'files_dp')):
+            print("files_dp exists: loading data")
+            self.r = np.load(os.path.join(self.path, 'files_dp/r.npy'))
+            self.t = np.load(os.path.join(self.path, 'files_dp/tdustev.npy'))
+            self.sigma_gas = np.load(os.path.join(self.path, 'files_dp/sigma_gas_2D.npy')) # (t,r)
+            self.sigma_dust_poly = np.load(os.path.join(self.path, 'files_dp/sigma_dust_3D.npy')) # (t,r,fluid-bins)
+            self.rho_gas = np.load(os.path.join(self.path, 'files_dp/rho_gas_2D.npy')) # (t,r)
+            self.rho_dust_poly = np.load(os.path.join(self.path, 'files_dp/rho_dust_3D.npy')) # (t,r,fluid-bins)
+            self.St_poly = np.load(os.path.join(self.path, 'files_dp/st_3D.npy')) # (t,r,fluid-bins)
+            self.temp = np.load(os.path.join(self.path, 'files_dp/temp_2D.npy')) # (t,r)
+            self.vrad_dust_poly = np.load(os.path.join(self.path, 'files_dp/vrad_dust_3D.npy')) # (t,r,fluid-bins)
+            print("Data has been loaded")
+        elif os.path.exists(os.path.join(self.path, 'data')):
+            print("data directory exists: reading and saving dustPy data")
+            os.mkdir(self.path+'/files_dp')
+            import dustpy
+            from dustpy import hdf5writer
+            wrtr = hdf5writer()
+            lent = self.findnrsave()+1
+            data = wrtr.read.output(0)
+            nrfluids = len(data.dust.Sigma[0,:])
+            self.r = data.grid.r
+            self.tdustev = np.zeros((lent))
+            self.sigma_gas_2D = np.zeros((lent,len(self.r)))
+            self.sigma_dust_3D = np.zeros((lent,len(self.r),nrfluids))
+            self.rho_gas_2D = np.zeros((lent,len(self.r)))
+            self.rho_dust_3D = np.zeros((lent,len(self.r),nrfluids))
+            self.vrad_dust_3D = np.zeros((lent,len(self.r),nrfluids))
+            self.temp_2D = np.zeros((lent,len(self.r)))
+            self.st_3D = np.zeros((lent,len(self.r),nrfluids))
+            for i in range(lent):
+                data = wrtr.read.output(i)
+                self.tdustev[i] = data.t
+                self.sigma_gas_2D[i,:] = data.gas.Sigma
+                self.sigma_dust_3D[i,:,:] = data.dust.Sigma
+                self.rho_gas_2D[i,:] = data.gas.rho
+                self.rho_dust_3D[i,:,:] = data.dust.rho
+                self.vrad_dust_3D[i,:,:] = data.dust.v.rad
+                self.temp_2D[i,:] = data.gas.T
+                self.st_3D[i,:,:] = data.dust.St
+            np.save(os.path.join(self.path,'files_dp/r.npy'),self.r)
+            np.save(os.path.join(self.path,'files_dp/tdustev.npy'),self.tdustev)
+            np.save(os.path.join(self.path,'files_dp/sigma_gas_2D.npy'),self.sigma_gas_2D)
+            np.save(os.path.join(self.path,'files_dp/sigma_dust_3D.npy'),self.sigma_dust_3D)
+            np.save(os.path.join(self.path,'files_dp/rho_gas_2D.npy'),self.rho_gas_2D)
+            np.save(os.path.join(self.path,'files_dp/rho_dust_3D.npy'),self.rho_dust_3D)
+            np.save(os.path.join(self.path,'files_dp/vrad_dust_3D.npy'),self.vrad_dust_3D)
+            np.save(os.path.join(self.path,'files_dp/temp_2D.npy'),self.temp_2D)
+            np.save(os.path.join(self.path,'files_dp/st_3D.npy'),self.st_3D)
+            print("Data has been loaded")
+        else:
+            raise FileNotFoundError("Error: no data exists in the specified path.")
 
     def limit(self):
         """
@@ -42,5 +87,18 @@ class Model:
         self.temp = self.temp[ind_t:,0:ind_r]
         self.vrad_dust_poly = self.vrad_dust_poly[ind_t:,0:ind_r,:]
 
-
-
+    def findnrsave(self):
+        """
+        Find nr of saved snapshots from dustPy
+        """
+        directory = os.path.join(self.path, 'data')
+        files = os.listdir(directory)
+        pattern = re.compile(r'data(\d+)\.hdf5')
+        indices = []
+        for filename in files:
+            match = pattern.match(filename)
+            if match:
+                indices.append(int(match.group(1)))
+        if indices:
+            max_index = max(indices)
+        return max_index
