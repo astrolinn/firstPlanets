6fe3e42e4da07663e565f6d444d976ef4b68a015
Linn Eriksson <linn@linns-mbp.internal.amnh.org>
2025-06-10 15:39:12 -0400

    Small updates

--- planetesimal.py
+++ planetesimal.py
@@ -1,8 +1,7 @@
-import numpy as np
 import astropy.constants as c
-from inputFile import (
-    MS,mu
-)
+import numpy as np
+
+import input_loader
 
 class Planetesimal:
     """
@@ -11,17 +10,20 @@ class Planetesimal:
     """
     def __init__(self, model, d2gSt):
         self.model = model
+        input_file = input_loader.load_input_file(self.model.path)
+        self.Mstar = input_file.Mstar
+        self.mu = input_file.mu
         self.d2gSt = d2gSt
         self.rocheDensity()
         self.massVortex()
-    
+
     def rocheDensity(self):
-        Omega = np.sqrt(c.G.cgs.value*MS/self.model.r**3)
-        self.rho_R = 9*Omega**2 / (4*np.pi*c.G.cgs.value)
+        Omega = np.sqrt(c.G.cgs.value * self.Mstar / self.model.r**3)
+        self.rho_R = 9 * Omega**2 / (4 * np.pi * c.G.cgs.value)
 
     def SI_Lim2024a(self, St):
         """
-        Calculates the critical dust-to-gas midplane 
+        Calculates the critical dust-to-gas midplane
         density ratio for the streaming instability
         to develop according to eq.16 of Lim et al.
         (2024a)
@@ -34,10 +36,10 @@ class Planetesimal:
         Calculates the critical dust-to-gas midplane 
         density ratio for the streaming instability
         to develop according to eq.15 of Lim et al.
-        (2024b)
+        (2024b) [Published version from 2025]
         A description of how the criteria is converted
         from surface densities to midplane densities is
-        found in Eriksson et al. (in prep.)
+        found in Eriksson et al. (2025)
         """
         Z_crit = 10**( 0.1 * np.log10(St)**2 + 0.07 * np.log10(St) - 2.36 )
         xi = 100 * Z_crit / np.sqrt(1 + St)
@@ -51,7 +53,7 @@ class Planetesimal:
         """
         pf = np.array((eps > eps_crit) | (eps * self.model.rho_gas > self.rho_R[None,:]), dtype=int)
         return pf
-    
+
     def planForm_roche(self, eps):
         """
         Check if the Roche density is reached
@@ -66,16 +68,16 @@ class Planetesimal:
         We assume a Kida vortex with chi=4
         """
         def _scaleFunction(chi):
-            omega_v = 3/2 / (chi - 1)
+            omega_v = 3 / (2 * (chi - 1))
             xi_plus = 1 + chi**(-2)
             f2chi = 2 * omega_v * chi - xi_plus**(-1) * (2 * omega_v**2 + 3)
             return np.sqrt(f2chi)
         chi = 4
         a = self.model.r
-        Omega = np.sqrt(c.G.cgs.value*MS/a**3)
+        Omega = np.sqrt(c.G.cgs.value * self.Mstar/ a**3)
         temp = self.model.temp
-        Cs = np.sqrt(c.k_B.cgs.value*temp/(mu*c.u.cgs.value))
-        H = Cs/Omega[None,:]
+        Cs = np.sqrt(c.k_B.cgs.value * temp / (self.mu * c.u.cgs.value))
+        H = Cs / Omega[None,:]
         rho_dust = self.model.rho_dust_poly.sum(-1)
         fchi = _scaleFunction(chi)
         Hg = H / fchi
@@ -83,35 +85,37 @@ class Planetesimal:
 
     def embryoMass_vortex(self, pf):
         """
-        Calculates the embryo mass using eq.X and assuming
-        that the embryo mass is 10x the standard mass of
-        a planetesimal forming via the SI
+        Calculates the embryo mass using eq.17 from Lorek
+        et al. (2022), originally from Liu et al. (2020),
+        and assuming that the embryo mass is 10x the
+        standard mass of a planetesimal forming via the SI
         Checks that embryo mass is not larger than the total
         mass of trapped dust in the vortex
         """
-        gamma = 1
         Zfil = 0.1
-        Omega = (c.G.cgs.value * MS / self.model.r**3)**(1/2)
-        Cs = (c.k_B.cgs.value * self.model.temp / (mu * c.u.cgs.value))**(1/2)
+        Omega = (c.G.cgs.value * self.Mstar / self.model.r**3)**(1/2)
+        Cs = (c.k_B.cgs.value * self.model.temp / (self.mu * c.u.cgs.value))**(1/2)
         H = Cs / Omega[None,:]
         h = H / self.model.r[None,:]
-        mp = 5e-5 * c.M_earth.cgs.value * (Zfil/ 0.02)**(1/2) * (gamma / np.pi**(-1))**(3/2) * (h / 0.05)**3 * (MS / c.M_sun.cgs.value)
+        gamma = 4 * np.pi * c.G.cgs.value * self.model.rho_gas * Omega**(-2)
+        mp = 5e-5 * c.M_earth.cgs.value * (Zfil / 0.02)**(1/2) * (gamma / np.pi**(-1))**(3/2) * (h / 0.05)**3 * (self.Mstar / c.M_sun.>
         membryo = 10 * mp * pf
         membryo = np.minimum(self.Mvortex, membryo)
         return membryo
     
     def embryoMass(self, pf):
         """
-        Calculates the embryo mass using eq.X and assuming
-        that the embryo mass is 10x the standard mass of
-        a planetesimal forming via the SI
+        Calculates the embryo mass using eq.17 from Lorek
+        et al. (2022), originally from Liu et al. (2020),
+        and assuming that the embryo mass is 10x the
+        standard mass of a planetesimal forming via the SI
         """
-        gamma = 1
         Zfil = 0.1
-        Omega = (c.G.cgs.value * MS / self.model.r**3)**(1/2)
-        Cs = (c.k_B.cgs.value * self.model.temp / (mu * c.u.cgs.value))**(1/2)
+        Omega = (c.G.cgs.value * self.Mstar / self.model.r**3)**(1/2)
+        Cs = (c.k_B.cgs.value * self.model.temp / (self.mu * c.u.cgs.value))**(1/2)
         H = Cs / Omega[None,:]
         h = H / self.model.r[None,:]
-        mp = 5e-5 * c.M_earth.cgs.value * (Zfil/ 0.02)**(1/2) * (gamma / np.pi**(-1))**(3/2) * (h / 0.05)**3 * (MS / c.M_sun.cgs.value)
+        gamma = 4 * np.pi * c.G.cgs.value * self.model.rho_gas * Omega**(-2)
+        mp = 5e-5 * c.M_earth.cgs.value * (Zfil / 0.02)**(1/2) * (gamma / np.pi**(-1))**(3/2) * (h / 0.05)**3 * (self.Mstar / c.M_sun.>
         membryo = 10 * mp * pf
         return membryo
