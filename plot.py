14e4825f20406052240483b1e872f9cf4cb657ae
Linn Elisabet Johanna Eriksson <leerikss@pfe24.nas.nasa.gov>
2025-06-16 16:58:35 -0700

    Modified colorbar range to match paper plots

--- plot.py
+++ plot.py
@@ -37,25 +37,25 @@ class Plot:
         # Stokes
         Stmin = 1e-4
         Stmax = 1
-        axes[0,0].pcolormesh(self.model.r/au, self.model.t/year, self.d2gSt.St_mono, norm=LogNorm(vmin=Stmin,vmax=Stmax))
-        axes[0,1].pcolormesh(self.model.r/au, self.model.t/year, self.d2gSt.St_mono, norm=LogNorm(vmin=Stmin,vmax=Stmax))
-        im0=axes[0,2].pcolormesh(self.model.r/au, self.model.t/year, self.d2gSt.St_vortex_growth, norm=LogNorm(vmin=Stmin,vmax=Stmax))
+        axes[0,0].pcolormesh(self.model.r/au, self.model.t/year, self.d2gSt.St_mono, norm=LogNorm(vmin=Stmin,vmax=Stmax), cmap='inferno')
+        axes[0,1].pcolormesh(self.model.r/au, self.model.t/year, self.d2gSt.St_mono, norm=LogNorm(vmin=Stmin,vmax=Stmax), cmap='inferno')
+        im0=axes[0,2].pcolormesh(self.model.r/au, self.model.t/year, self.d2gSt.St_vortex_growth, norm=LogNorm(vmin=Stmin,vmax=Stmax), cmap='inferno')
         cbar0 = fig.colorbar(im0, ax=axes[0, :], orientation='vertical', fraction=0.2, pad=0.02)
         cbar0.set_label('St')
         # Dust-to-gas ratios
-        d2gmin = 5e-4
+        d2gmin = 1.1e-3
         d2gmax = 5e3
-        axes[1,0].pcolormesh(self.model.r/au, self.model.t/year, self.d2gSt.d2gRho, norm=LogNorm(vmin=d2gmin,vmax=d2gmax))
-        axes[1,1].pcolormesh(self.model.r/au, self.model.t/year, self.d2gSt.d2gRho_vortex, norm=LogNorm(vmin=d2gmin,vmax=d2gmax))
-        im1=axes[1,2].pcolormesh(self.model.r/au, self.model.t/year, self.d2gSt.d2gRho_vortex_growth, norm=LogNorm(vmin=d2gmin,vmax=d2gmax))
+        axes[1,0].pcolormesh(self.model.r/au, self.model.t/year, self.d2gSt.d2gRho, norm=LogNorm(vmin=d2gmin,vmax=d2gmax), cmap='hot')
+        axes[1,1].pcolormesh(self.model.r/au, self.model.t/year, self.d2gSt.d2gRho_vortex, norm=LogNorm(vmin=d2gmin,vmax=d2gmax), cmap='hot')
+        im1=axes[1,2].pcolormesh(self.model.r/au, self.model.t/year, self.d2gSt.d2gRho_vortex_growth, norm=LogNorm(vmin=d2gmin,vmax=d2gmax), cmap='hot')
         cbar1 = fig.colorbar(im1, ax=axes[1, :], orientation='vertical', fraction=0.2, pad=0.02)
         cbar1.set_label(r'$\epsilon$')
         # Embryo masses
-        emin = 0
-        emax = 0.03
-        axes[2,0].pcolormesh(self.model.r/au, self.model.t/year, np.where(membryo == 0, np.nan, membryo / ME), vmin=emin, vmax=emax)
-        axes[2,1].pcolormesh(self.model.r/au, self.model.t/year, np.where(membryo_vortex == 0, np.nan, membryo_vortex / ME), vmin=emin, vmax=emax)
-        im2=axes[2,2].pcolormesh(self.model.r/au, self.model.t/year, np.where(membryo_vortex_growth == 0, np.nan, membryo_vortex_growth / ME), vmin=emin, vmax=emax)
+        emin = 1e-6
+        emax = 2.5e-4
+        axes[2,0].pcolormesh(self.model.r/au, self.model.t/year, np.where(membryo == 0, np.nan, membryo / ME), norm=LogNorm(vmin=emin,vmax=emax))
+        axes[2,1].pcolormesh(self.model.r/au, self.model.t/year, np.where(membryo_vortex == 0, np.nan, membryo_vortex / ME), norm=LogNorm(vmin=emin,vmax=emax))
+        im2=axes[2,2].pcolormesh(self.model.r/au, self.model.t/year, np.where(membryo_vortex_growth == 0, np.nan, membryo_vortex_growth / ME), norm=LogNorm(vmin=emin,vmax=emax))
         cbar2 = fig.colorbar(im2, ax=axes[2, :], orientation='vertical', fraction=0.2, pad=0.02)
         cbar2.set_label(r'$M_{\rm embryo}/\rm{M}_{\oplus}$')
         # Labels
@@ -100,24 +100,24 @@ class Plot:
         plt.subplots_adjust(wspace=0.05)
         plt.subplots_adjust(hspace=0.05)
         # Embryo masses
-        emin = 0
-        emax = 0.03
-        axes[0,0].pcolormesh(self.model.r/au,self.model.t/year,np.where(membryo_vortex == 0, np.nan, membryo_vortex / ME),vmin=emin,vmax=emax)
-        im0=axes[0,1].pcolormesh(self.model.r/au,self.model.t/year,np.where(membryo_vortex_growth == 0, np.nan, membryo_vortex_growth / ME),vmin=emin,vmax=emax)
+        emin = 1e-6
+        emax = 0.3
+        axes[0,0].pcolormesh(self.model.r/au,self.model.t/year,np.where(membryo_vortex == 0, np.nan, membryo_vortex / ME),norm=mcolors.LogNorm(vmin=emin, vmax=emax))
+        im0=axes[0,1].pcolormesh(self.model.r/au,self.model.t/year,np.where(membryo_vortex_growth == 0, np.nan, membryo_vortex_growth / ME),norm=mcolors.LogNorm(vmin=emin, vmax=emax))
         cbar0 = fig.colorbar(im0, ax=axes[0, :], orientation='vertical', fraction=0.2, pad=0.02)
         cbar0.set_label(r'$M_{\rm embryo}/\rm{M}_{\oplus}$') 
         # Planetary masses when accretion is purely outside the vortex
-        pmin = 0
-        pmax = 0.03
-        axes[1,0].pcolormesh(rform_exact/au,tform_exact/year,np.where(mp_vortex == 0, np.nan, mp_vortex / ME),vmin=pmin,vmax=pmax)
-        im1=axes[1,1].pcolormesh(rform_exact/au,tform_exact/year,np.where(mp_vortex_growth == 0, np.nan, mp_vortex_growth / ME),vmin=pmin,vmax=pmax)
+        pmin = 1e-6
+        pmax = 0.3
+        axes[1,0].pcolormesh(rform_exact/au,tform_exact/year,np.where(mp_vortex == 0, np.nan, mp_vortex / ME),norm=mcolors.LogNorm(vmin=pmin, vmax=pmax))
+        im1=axes[1,1].pcolormesh(rform_exact/au,tform_exact/year,np.where(mp_vortex_growth == 0, np.nan, mp_vortex_growth / ME),norm=mcolors.LogNorm(vmin=pmin, vmax=pmax))
         cbar1 = fig.colorbar(im1, ax=axes[1, :], orientation='vertical', fraction=0.2, pad=0.02)
         cbar1.set_label(r'$M_{\rm p}/\rm{M}_{\oplus}$ - 0P in vortex') 
         # Planetary masses when accretion is initially inside the vortex
-        pmin = 0
+        pmin = 1e-6
         pmax = 0.3
-        axes[2,0].pcolormesh(rform_exact/au,tform_exact/year,np.where(mp_vortex_2 == 0, np.nan, mp_vortex_2 / ME),vmin=pmin,vmax=pmax)
-        im2=axes[2,1].pcolormesh(rform_exact/au,tform_exact/year,np.where(mp_vortex_growth_2 == 0, np.nan, mp_vortex_growth_2 / ME),vmin=pmin,vmax=pmax)
+        axes[2,0].pcolormesh(rform_exact/au,tform_exact/year,np.where(mp_vortex_2 == 0, np.nan, mp_vortex_2 / ME),norm=mcolors.LogNorm(vmin=pmin, vmax=pmax))
+        im2=axes[2,1].pcolormesh(rform_exact/au,tform_exact/year,np.where(mp_vortex_growth_2 == 0, np.nan, mp_vortex_growth_2 / ME),norm=mcolors.LogNorm(vmin=pmin, vmax=pmax))
         cbar2 = fig.colorbar(im2, ax=axes[2, :], orientation='vertical', fraction=0.2, pad=0.02)
         cbar2.set_label(r'$M_{\rm p}/\rm{M}_{\oplus}$ - 10P in vortex') 
         # Labels
@@ -129,5 +129,16 @@ class Plot:
         axes[0,0].set_xlim([1,50])
         axes[0,0].set_ylim([0,3e6])
         plt.savefig(os.path.join(self.path,'planet.png'),bbox_inches='tight')
-        plt.show()
+        #plt.show()
         plt.close()
+
+        np.save(os.path.join(self.path, 'plots', 'rform.npy'), rform_exact)
+        np.save(os.path.join(self.path, 'plots', 'tform.npy'), tform_exact)
+        np.save(os.path.join(self.path, 'plots', 'mp_vortex.npy'), mp_vortex)
+        np.save(os.path.join(self.path, 'plots', 'mp_vortex_growth.npy'), mp_vortex_growth)
+        np.save(os.path.join(self.path, 'plots', 'mp_vortex_no10.npy'), mp_vortex_2)
+        np.save(os.path.join(self.path, 'plots', 'mp_vortex_growth_no10.npy'), mp_vortex_growth_2)
+        np.save(os.path.join(self.path, 'plots', 'mp_vortex_a.npy'), mp_vortex_a)
+        np.save(os.path.join(self.path, 'plots', 'mp_vortex_growth_a.npy'), mp_vortex_growth_a)
+        np.save(os.path.join(self.path, 'plots', 'mp_vortex_no10_a.npy'), mp_vortex_2_a)
+        np.save(os.path.join(self.path, 'plots', 'mp_vortex_growth_no10_a.npy'), mp_vortex_growth_2_a)
